337358e4aa8d02bf190f60f8cdb506c27e0fcd54
Valar Morghulis <524672142@qq.com>
2016-08-03 21:27:43 +0800

    update

--- 08/文本文件切割与合并/文本文件切割与合并/大文本数据切割与合并.c
+++ 08/文本文件切割与合并/文本文件切割与合并/大文本数据切割与合并.c
@@ -32,9 +32,63 @@ void space(char *path, int num) {
 		pathes[i] = malloc(sizeof(char) * 256);
 		sprintf(pathes[i], "G:\\BigData.txt%d", i + 1);
 		printf("\n%s", pathes[i]);
-
 	}
+
+	//100 10 10
+	//100 9	8*12+4
+	FILE *pf = fopen(path, "r");
+
+	if (pf == NULL) {
+		return -1;
+	}
+	else {
+		if (N%num == 0) {
+			//num个,N/num
+			for (int i = 0; i < num; i++) {
+				FILE *pfw = fopen(path[i], "w");			//写入
+				for (int j = 0; j < N / num; j++) {
+					char str[1024] = { 0 };
+					fgets(str, 1024, pf);
+					fputs(str, pfw);					//读取一行写入一行
+				}
+				fclose(pfw);
+			}
+		}
+		else {
+
+			//100	9	8*12+4
+
+			for (int i = 0; i < num - 1; i++) {
+				FILE *pfw = fopen(path[i], "w");			//写入
+				for (int j = 0; j < N / (num - 1); j++) {
+					char str[1024] = { 0 };
+					fgets(str, 1024, pf);
+					fputs(str, pfw);					//读取一行写入一行
+				}
+				fclose(pfw);
+			}
+
+			{
+				FILE *pfw = fopen(path[num - 1], "w");			//写入
+				for (int j = 0; j < N % (num - 1); j++) {
+					char str[1024] = { 0 };
+					fgets(str, 1024, pf);
+					fputs(str, pfw);					//读取一行写入一行
+				}
+				fclose(pfw);
+			}
+
+
+		}
+		fclose(pf);
+	}
+
 }
+
+void merge(char *newpath) {
+
+}
+
 
 void main() {
 
