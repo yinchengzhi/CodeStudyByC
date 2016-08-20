8326f6762b9b94a4c4274b5d5b3ecdc76b4f1a94
Valar Morghulis <524672142@qq.com>
2016-08-20 19:33:45 +0800

    update

--- 08/内存排序/内存排序/排序.c
+++ 08/内存排序/内存排序/排序.c
@@ -37,15 +37,22 @@ int com(void*p1, void *p2) {
 	}
 }
 
+
+//读取到内存，然后排序
 void memsort() {
+
 	int *p = malloc(400);
-	FILE *pf = fopen("G:\\test.data", "rb");
+	int *pread = malloc(400);
+
+	FILE *pf = fopen("G:\\test.data", "rb+");
 	fread(p, sizeof(int), 100, pf);
-	fclose(pf);
 
 	for (int i = 0; i < 100; i++) {
 		printf("\n%d", p[i]);
 	}
+
+	fflush(pf);							//刷新 生效
+
 	qsort(p, 100, 4, com);				//排序
 
 	printf("sort later\n");
@@ -53,6 +60,20 @@ void memsort() {
 	for (int i = 0; i < 100; i++) {
 		printf("\n%d", p[i]);
 	}
+
+	rewind(pf);
+	fwrite(p, sizeof(int), 100, pf);
+	fflush(pf);							//刷新 生效
+
+	rewind(pf);
+	fread(pread, sizeof(int), 100, pf);
+	fflush(pf);							//刷新 生效
+
+	for (int i = 0; i < 100; i++) {
+		printf("\npread=%d", pread[i]);
+	}
+
+	fclose(pf);
 }
 
 void main() {
