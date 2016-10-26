600df2a7fdf4cfd2b5f31f24be77c4c81aa72c3c
Valar Morghulis <524672142@qq.com>
2016-10-26 22:31:43 +0800

    update

--- 10/pipe/pipe/client.c
+++ 10/pipe/pipe/client.c
@@ -4,6 +4,19 @@
 //限定程序 只能打开
 #define SIZE 4096
 char pipename[128] = "yinchengpipe";
+HANDLE m_pipe = NULL;							//管道的句柄
+
+void start() {
+	m_pipe = CreateNamedPipeA(pipename,							//管道名称
+		PIPE_ACCESS_DUPLEX,										//管道读写属性
+		PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,	//消息模式，读模式，等待模式阻塞
+		PIPE_UNLIMITED_INSTANCES,								//最大个数
+		SIZE,													//输出缓冲区的大小
+		SIZE,													//输入缓冲区的大小
+		0,														//超时，无限等待
+		NULL);
+
+}
 
 void test() {
 	HANDLE mutex = OpenMutexA(MUTEX_ALL_ACCESS, TRUE, "yinchengzhiserver");
@@ -22,7 +35,7 @@ void test() {
 void main() {
 
 	test();
-
+	start();
 
 	system("pause");
 }
