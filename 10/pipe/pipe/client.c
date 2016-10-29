abb4d9dd66b315a07b3506328efb13b9d336ee1e
Valar Morghulis <524672142@qq.com>
2016-10-29 13:48:10 +0800

    update

--- 10/pipe/pipe/client.c
+++ 10/pipe/pipe/client.c
@@ -3,63 +3,50 @@
 
 //限定程序 只能打开
 #define SIZE 4096
-char pipename[128] = "\\\\. \\Pipe\\yinchengzhiPipe";
+char pipename[128] = "\\\\.\\Pipe\\yinchengzhiPipe";
 HANDLE m_pipe = NULL;							//管道的句柄
-
-void start() {
-	m_pipe = CreateNamedPipeA(pipename,							//管道名称
-		PIPE_ACCESS_DUPLEX,										//管道读写属性
-		PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,	//消息模式，读模式，等待模式阻塞
-		PIPE_UNLIMITED_INSTANCES,								//最大个数
-		SIZE,													//输出缓冲区的大小
-		SIZE,													//输入缓冲区的大小
-		0,														//超时，无限等待
-		NULL);
-
-	if (m_pipe == NULL) {
-		printf("创建失败!");
-	}
-
-	BOOL isconnect = ConnectNamedPipe(m_pipe, NULL) ? TRUE : (GetLastError() == ERROR_PIPE_CONNECTED);		//是否连上
-	if (isconnect) {
-		MessageBoxA(0, "connected ok", "connected ok", 0);
-	}
-	else {
-		printf("连接失败");
-	}
-
-}
-
-void test() {
-	HANDLE mutex = OpenMutexA(MUTEX_ALL_ACCESS, TRUE, "yinchengzhiserver");
-	if (mutex == NULL) {
-		mutex = CreateMutexA(NULL, TRUE, "yinchengzhiserver");
-
-	}
-	else {
-		MessageBoxA(0, "only one", "only one", 0);
-		exit(0);
-	}
-
-}
 
 void read() {
 	char buf[SIZE] = { 0 };
 	int last;
-	if (ReadFile(m_pipe, buf, SIZE, &last,NULL)) {
-
+	if (!ReadFile(m_pipe, buf, SIZE, &last, NULL)) {
+		printf("读取失败");
+		return;
 	}
 
+	//buf[last] = '\0';
+
+	printf("\nread=%s", buf);
 }
 
 void write() {
-
+	char str[128] = "扯淡，好好看书";
+	int last = 0;
+	BOOL res = WriteFile(m_pipe, str, sizeof(str), &last, NULL);
+	if (!res)
+	{
+		printf("写入失败");
+	}
 }
 
 void main() {
 
-	test();
-	start();
+	//NMPWAIT_USE_DEFAULT_WAIT 无限等待
+	if (!WaitNamedPipeA(pipename, NMPWAIT_USE_DEFAULT_WAIT)) {
+		MessageBoxA(0, "connected No", "connected No", 0);
+		return;
+	}
+	m_pipe = CreateFileA(pipename,				//名称
+		GENERIC_WRITE | GENERIC_READ,			//读写
+		1,										//共享属性
+		NULL,									//安全属性
+		OPEN_EXISTING,							//打开已经存在的
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
 
+	system("pause");
+	read();
+	system("pause");
+	write();
 	system("pause");
 }
