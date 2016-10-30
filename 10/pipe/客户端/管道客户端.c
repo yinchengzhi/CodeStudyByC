1a07f3db6c34788dd1f7ab66a89b1fdd133a760c
Valar Morghulis <524672142@qq.com>
2016-10-30 22:07:30 +0800

    update

---
+++ 10/pipe/客户端/管道客户端.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<Windows.h>
+
+//限定程序 只能打开
+#define SIZE 4096
+char pipename[128] = "\\\\.\\Pipe\\cloudpipe";
+HANDLE m_pipe = NULL;							//管道的句柄
+
+void main() {
+	m_pipe = CreateFileA(pipename,				//名称
+		GENERIC_WRITE | GENERIC_READ,			//读写
+		1,										//共享属性
+		NULL,									//安全属性
+		OPEN_EXISTING,							//打开已经存在的
+		FILE_ATTRIBUTE_NORMAL,
+		NULL);
+
+
+
+	system("pause");
+}
