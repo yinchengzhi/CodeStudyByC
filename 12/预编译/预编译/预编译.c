d0a7faf2aaaf64fc7f27cbf2de91964c22b0c090
Valar Morghulis <524672142@qq.com>
2017-01-15 22:21:17 +0800

    update

--- 12/预编译/预编译/预编译.c
+++ 12/预编译/预编译/预编译.c
@@ -1,11 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+
 
 #define runfollowme main
 
+void add(int a, int b);				//载入函数的实体
+
 void runfollowme() {
 
+	int num;						//传递地址,不保留
+	//01 0x300500
+	//putitout(num);
+	//"num"
 
+	printf("hello world");
+	add(10, 20);					//呼叫某一个函数
 
 	system("pause");
 }
